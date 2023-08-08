a7adef5e7ecbf355a1de31c606fe3b9b0b43cf39
Aidan <aidan.lowson@davies-group.com>
2023-08-08 12:17:37 +0100

    Slot logic

--- main.py
+++ main.py
@@ -1,7 +1,62 @@
+import random
+
 MAX_LINES = 3 # Global const (all caps is the convention in Python for defining global variables)
 MAX_BET = 100
 MIN_BET = 1
 
+# Rows and Cols for the slot machine
+ROWS = 3
+COLS = 3
+
+# The symbols that are on each column of the slot machine
+symbol_count = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+# The multipliers for winning on different symbols
+symbol_value = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+def check_winnings(columns, lines, bet):
+
+  
+def get_slot_machine_spin(rows, cols, symbols):
+  all_symbols = []
+  for symbol, symbol_count in symbols.items(): # Loop through each symbol in our symbol_count dictionary
+    for _ in range(symbol_count): # _ is used in place of the iterator var, just so we don't have an unused var
+      all_symbols.append(symbol) # Will add the symbol as many times as the symbol count, so there will be 2 As etc.
+      
+  columns = []
+  for _ in range(cols):
+    column = []
+    current_symbols = all_symbols[:] # : means copy all_symbols, so we can mutate current_symbols
+
+    for _ in range(rows): #for every column, we need to generate a certain amount of symbols
+      value = random.choice(all_symbols)
+      current_symbols.remove(value) # Remove from current symbols
+      column.append(value)
+    columns.append(column)
+  
+  return columns
+
+def print_slot_machine(columns):
+  # Need to transpose as this will be a matrix (to get the column slots to display properly)
+  for row in range(len(columns[0])):
+    for i, column in enumerate(columns):
+      if i != len(columns) - 1:
+        print(column[row], end=" | ")
+      else:
+        print(column[row], end="")
+        
+    print() # Print on a new line
+    
 def deposit():
   while True: # Keep asking for a deposit until a valid deposit is entered
     amount = input("How much do you want to deposit? £")
@@ -51,6 +106,9 @@ def main():
       print(f"You do not have a sufficient deposit to bet that amount, your current balance is: £{balance}")
     else:
       break
+    
+  slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+  print_slot_machine(slots)
   
   
   print(f"You are betting £{bet} on £{lines}, total bet is £{total_bet}. Your current balance is £{balance}")
